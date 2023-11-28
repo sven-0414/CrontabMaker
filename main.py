c800ae6d6523aa39ad41c6a3b44135ad1c425daf
sven-0414 <sveneriksson@icloud.com>
2023-11-28 19:02:12 +0100

    första metoden för att hämta matchdatan

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
+import sys
 
 
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Skriptet behöver ett argument för seriens idnummer.")
+    else:
+        liga_id = sys.argv[1]
+        resultat = hamta_matchdata(liga_id)
+        print(resultat)
+
+
+def hamta_matchdata(liga_id):
+    try:
+        url = f"https://stats.swehockey.se/ScheduleAndResults/Schedule/{liga_id}"
+        response = requests.get(url)
+        response.raise_for_status()  # Detta kommer att kasta ett undantag om statuskoden inte är 200
+        return response.text         # eller response.json() om svaret är JSON
+    except requests.RequestException as e:
+        return f"Kunde inte hämta data: {e}"
